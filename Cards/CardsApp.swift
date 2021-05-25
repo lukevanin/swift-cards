d1a2f129b9ac1696db842f94528076a30d4290b5
Luke Van In <lukevanin@gmail.com>
2021-05-25 20:58:26 +0200

    Initial commit
    Add project skeleton and graphics source files

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Luke Van In on 2021/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
